16198aba08e47242d281686819cc6dfc89093858
Vladimir Munteanu <vlmu@trackman.com>
2026-06-29 17:28:25 +0200

    wip, n/o

---
+++ Assets/Source/Planets/Profile/ProfileLayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Planets.Profiles
+{
+    public abstract class ProfileLayer : ScriptableObject
+    {
+        public bool active = true;
+
+        public abstract void Evaluate(ref Sample sample);
+    }
+}
---
+++ Assets/Source/Planets/Profile/Sample.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Planets.Profiles
+{
+    public struct Sample
+    {
+        public Vector3 pointOnSphere;
+        public Vector3 normal;
+        public float   radius;
+
+        public TectonicPlateBlock tectonicPlate;
+        
+        public Sample(Vector3 pointOnSphere, float radius)
+        {
+            this.pointOnSphere = pointOnSphere;
+            this.radius = radius;
+            this.normal = pointOnSphere.normalized;
+        }
+
+
+        public struct TectonicPlateBlock
+        {
+            public enum EBoundary { None, Divergent, Convergent, Slide }
+            
+            public bool hasData;
+            public int plateID;
+            public int secondPlateID;
+            public float plateDistance;
+            public float secondPlateDistance;
+            public float boundaryStrength;
+            public EBoundary boundaryType;
+        }
+    }
+}
